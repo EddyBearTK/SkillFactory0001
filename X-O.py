77e3137002826c83366eb1646d942942fe026415
EddyBearTK <edgar.tkachev@gmail.com>
2023-03-02 20:59:45 +0300

    Update X-O.py

--- X-O.py
+++ X-O.py
@@ -1,12 +1,34 @@
 print("Приветствую в игре Крестики-нолики (X-O)!")
-#input('Введи адрес ячейки, где хочешь поставить Х в формате: 1 цифра - ряд, 2 цифра - колонка')
 
+#Начальная установка полей для ввода данных
 fields = [['-']*3 for _ in range(3)]
 
+#Приводим поле в адекватный внешний вид
 def show_fields(f):
-
     print('  0 1 2')
     for i in range(len(fields)):
         print(str(i)+' '+' '.join(fields[i]))
 show_fields(fields)
 
+#Задаем вопрос по координатам для ввода координат:
+def users_input(f):
+    while True:
+        place=input("Введите координаты для поля:").split()
+        if len(place) != 2:
+            print('Необходимо ввести две координаты')
+            continue
+        
+    
+        if not(place[0].isdigit() and place[1].isdigit()):
+            print("Введите числа")
+            continue
+        
+        x,y = map(int, map)
+        
+        
+        
+        
+        break
+    return place
+
+users_input(fields)
