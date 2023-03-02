32b3be1728b0245a54b8d5ec3538c3f83b7a5349
EddyBearTK <edgar.tkachev@gmail.com>
2023-03-02 21:34:51 +0300

    Update X-O.py

--- X-O.py
+++ X-O.py
@@ -1,7 +1,7 @@
 print("Приветствую в игре Крестики-нолики (x-o)!")
 
 def show_field(f):
-    num ='  0 1 2'
+    num = '  0 1 2'
     print(num)
     #zip
     for row,i in zip(f,num.split()):
@@ -9,7 +9,7 @@ def show_field(f):
 
 def users_input(f,user):
     while True:
-        place=input(f"Ходит {user} .Введите координаты:").split()
+        place = input(f"Ходит {user} .Введите координаты:").split()
         if len(place)!=2:
             print('Введите две координаты')
             continue
@@ -18,10 +18,10 @@ def users_input(f,user):
             print('Введите числа')
             continue
         x, y = map(int, place)
-        if not(x>=0 and x<3 and y>=0 and  y<3):
+        if not(x >= 0 and x < 3 and y >= 0 and  y < 3):
             print('Вышли из диапазона')
             continue
-        if f[x][y]!='-':
+        if f[x][y] != '-':
             print('Клетка занята')
             continue
         break
@@ -29,12 +29,12 @@ def users_input(f,user):
 
 
 def win_position(f,user):
-    f_list=[]
+    f_list = []
     print(f)
     for l in f:
-        f_list+=l
+        f_list += l
     print(f_list)
-    positions=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    positions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     indices = set([i for i, x in enumerate(f_list) if x == user])
 
     for p in positions:
@@ -48,21 +48,21 @@ def start(field):
     count=0
     while True:
         show_field(field)
-        if count%2==0:
-            user='x'
+        if count%2 == 0:
+            user = 'x'
         else:
             user = 'o'
-        if count<9:
+        if count < 9:
             x, y = users_input(field,user)
             field[x][y] = user
 
-        elif count==9:
+        elif count == 9:
             print ('Ничья')
             break
         if win_position(field,user):
             print(f"Выйграл {user}")
             break
-        count+=1
+        count += 1
 
 
 field = [['-'] * 3 for _ in range(3)]
